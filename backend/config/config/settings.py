db9399ead8657818ba6a133f704b1ca9b2a13068
tusharNova <tusharmankar02@gmail.com>
2026-07-09 23:55:39 +0530

    feat: add new apps in project

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # app
+    "account",
+    "common",
+    "monitoring",
 ]
 
 MIDDLEWARE = [
